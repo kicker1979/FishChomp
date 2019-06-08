06c1c2fd872e95722722bb7377065f8669c47e19
Craig R Harrell <kick12380@yahoo.com>
2019-06-08 14:34:57 -0400

    Initial commit

---
+++ app/src/main/java/mobi/hephaestus/fishchomp/OnBoardingScreen.java
@@ -0,0 +1,13 @@
+package mobi.hephaestus.fishchomp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnBoardingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding_screen);
+    }
+}
